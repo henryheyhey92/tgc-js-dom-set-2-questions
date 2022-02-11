da30801714d0c4ab5dc5be782820bfab654939ac
Henry Lai <e0032134@u.nus.edu>
2022-02-11 00:37:11 +0000

    add files for basic

--- basic/r3q1_Selection_Redux/script.js
+++ basic/r3q1_Selection_Redux/script.js
@@ -1,3 +1,20 @@
 // PLACE YOUR CODE HERE
+//1.
+let h2 = document.querySelector('h2');
+h2.style.fontSize = "32px";
+h2.style.fontFamily = "Verdana";
 
 
+//2.
+let img = document.querySelector('img');
+img.style.border = "1px solid black";
+img.style.paddingTop = "32px";
+img.style.paddingBottom = "15px";
+
+
+//3.
+let facts = document.querySelectorAll('ul>li');
+let first = facts.firstChild;
+let last = facts.lastElementChild;
+first.style.backgroundColor = "yellow";
+last.style.backgroundColor = "yellow";
